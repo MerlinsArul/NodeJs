b46c713d0bed285f4535a46d6abd04440804f597
MerlinsArul <merlins.arul@aspiresys.com>
2023-03-07 16:51:49 +0530

    nodejs

--- eventex.js
+++ eventex.js
@@ -5,8 +5,8 @@ const emitter = new EventEmitter();
 emitter.on("event is begin", (number, name) => {
   console.log(`Event start to run as ${number} and with name ${name}`);
 });
-// emitter.on("event is begin", (number) => {
-//   if (number == "event1") console.log("This is number 1 event");
-// });
-// console.log("code before event emission");
+emitter.on("event is begin", (number) => {
+  if (number == "event1") console.log("This is number 1 event");
+});
+console.log("code before event emission");
 emitter.emit("event is begin", "event1", "eventemitter");
--- main.mjs
+++
@@ -1,6 +0,0 @@
-import math from "./math.mjs";
-
-const {add,sub} = math;
-
-console.log(add(5,5));
-console.log(sub(7,4));
